97f9e4a9dae009c88f42d8b462729bda887d9b69
David Hofmann <git@davids-mailbox.com>
2025-03-07 02:52:08 +0000

    Move all file load steps into parse-import-file-rule.ts

--- src/rules/parse/parse-import-file-rule.ts
+++ src/rules/parse/parse-import-file-rule.ts
@@ -1,8 +1,11 @@
+import { readFileSync } from "fs";
 import { Config } from "../../types/config.js";
 import { Rule } from "../../types/rules.js";
+import { fail } from "../../utils/fail.js";
 import { createFileDescriptor } from "../helpers/create-file-descriptor.js";
 import { comesFromFile } from "../helpers/rule-type-guards.js";
-import { loadFile } from "../load/load-file.js";
+import { assertFileExists } from "../validate/file-exists.js";
+import { assertNoCyclicImports } from "../validate/no-cyclic-imports.js";
 import { parseRules } from "./parse-rules.js";
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -73,4 +76,43 @@ function getStringified(operator: string | undefined, file: Rule.Fragment.File) 
         original: `${operator} ${file.original}`,
         effective: `${operator} ${file.resolved}`,
     };
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Load and parse given file
+//----------------------------------------------------------------------------------------------------------------------
+
+function loadFile(parent: Rule.ImportFile, file: Rule.Fragment.File): ReadonlyArray<Rule.Source.File> {
+    assertFileExists(parent.source, file);
+    assertNoCyclicImports(parent);
+    return loadLines(parent.source, file)
+        .map((line, index) => ({ file, line, lineNumber: index + 1 }))
+        .filter(item => !/^\s*(#.*)?$/.test(item.line))
+        .map(item => ({ ...item, indentation: -1, parent: parent.source, type: "file" }) as const)
+        .map(item => ({ ...item, indentation: getIndentation(item) }));
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Parse a single file
+//----------------------------------------------------------------------------------------------------------------------
+
+function loadLines(source: Rule.Source, file: Rule.Fragment.File) {
+    try {
+        return readFileSync(file.resolved).toString().split(/\r?\n/);
+    } catch (error) {
+        return fail(source, `Failed to load file ${file.absolute}\n${error}`);
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Calculate the indentation
+//----------------------------------------------------------------------------------------------------------------------
+
+function getIndentation(rule: Rule.Source.File) {
+    const leadingWhitespace = rule.line.replace(/[^\s].*$/, "");
+    const leadingArrows = (/^[^\sa-z\d]*</i.exec(rule.line.trim())?.[0] ?? "").replace(/<$/, "");
+    if (0 <= leadingWhitespace.indexOf("\t")) {
+        fail(rule, "The line contains leading tabs. Rules must be indented with spaces only.");
+    }
+    return leadingWhitespace.length + leadingArrows.length;
 }
